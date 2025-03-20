e22b61a533e5db9f1b0532a80f0b293c70279a54
Abenezer Assefa <94551524+AbenezerAssefa@users.noreply.github.com>
2025-03-20 13:10:40 +0100

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using assignemtforC.Entities;
+using assignemtforC.Enum;
+
+public class Food
+{
+    public List<FoodItem> Items { get; private set; }
+
+    public Food(int maxX, int maxY, Random random)
+    {
+        Items = new List<FoodItem>();
+        GenerateNewItems(maxX, maxY, random);
+    }
+
+    public void GenerateNewItems(int maxX, int maxY, Random random)
+    {
+        Items.Clear();
+
+        // Generate one regular berry and one poisoned berry
+        Items.Add(new FoodItem(new Point2D(random.Next(1, maxX - 2), random.Next(1, maxY - 2)), FoodType.Normal));
+        Items.Add(new FoodItem(new Point2D(random.Next(1, maxX - 2), random.Next(1, maxY - 2)), FoodType.Poisoned));
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using assignemtforC.Entities;
+using assignemtforC.Enum;
+
+public class Snake
+{
+    public List<Point2D> Body { get; private set; }
+    public Direction Direction { get; private set; }
+
+    public Snake(int startX, int startY, ConsoleColor color, int initialLength)
+    {
+        Body = new List<Point2D>();
+        for (int i = 0; i < initialLength; i++)
+        {
+            Body.Add(new Point2D(startX - i, startY));
+        }
+        Direction = Direction.Right;
+    }
+
+    public void Move()
+    {
+        var head = Body.Last();
+        int newX = head.X;
+        int newY = head.Y;
+
+        switch (Direction)
+        {
+            case Direction.Up:
+                newY--;
+                break;
+            case Direction.Down:
+                newY++;
+                break;
+            case Direction.Left:
+                newX--;
+                break;
+            case Direction.Right:
+                newX++;
+                break;
+        }
+
+        Body.Add(new Point2D(newX, newY));
+        Body.RemoveAt(0);
+    }
+
+    public void ChangeDirection(Direction newDirection)
+    {
+        // Prevent reversing direction
+        if ((Direction == Direction.Up && newDirection != Direction.Down) ||
+            (Direction == Direction.Down && newDirection != Direction.Up) ||
+            (Direction == Direction.Left && newDirection != Direction.Right) ||
+            (Direction == Direction.Right && newDirection != Direction.Left))
+        {
+            Direction = newDirection;
+        }
+    }
+
+    public void Grow()
+    {
+        var tail = Body.First();
+        Body.Insert(0, new Point2D(tail.X, tail.Y));
+    }
+}
